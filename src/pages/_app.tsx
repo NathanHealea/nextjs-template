a742d005630627f4069d203ca9bc667078e07980
Nathan Healea <nate.healea@gmail.com>
2022-12-05 09:30:45 -0800

    v1.1.0: Implement Mui, Mui Icons, and necessary dependencies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,11 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+// Styles
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import '../styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
